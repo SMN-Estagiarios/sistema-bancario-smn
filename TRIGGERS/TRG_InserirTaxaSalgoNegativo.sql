7a298d4082eb86eeede176bd3878c224ae553e5c
odlavir <lavir.andrade@gmail.com>
2024-04-15 14:00:51 -0300

    Eliminando a recursividade que estava acontecendo no trigger inserirtaxasaldonegativo

--- TRIGGERS/TRG_InserirTaxaSalgoNegativo.sql
+++ TRIGGERS/TRG_InserirTaxaSalgoNegativo.sql
@@ -3,9 +3,9 @@ CREATE OR ALTER TRIGGER [DBO].[TRG_InserirTaxaSalgoNegativo]
 	AFTER UPDATE
 	AS
 	 /*
-		Documenta��o
+		Documentação
 		Arquivo Fonte.....:  TRG_InserirTaxaSalgoNegativo.sql
-		Objetivo.............: Verificar diariamente quais contas est�o negativas e aplicar a taxa de saldo negativo para a conta que estiver
+		Objetivo.............: Verificar diariamente quais contas estão negativas e aplicar a taxa de saldo negativo para a conta que estiver
 		Autor.................: Orcino Neto, Odlavir Florentino e Pedro Avelino
 		Data..................: 12/04/2024
 		Ex.....................:		 BEGIN TRAN
@@ -17,9 +17,12 @@ CREATE OR ALTER TRIGGER [DBO].[TRG_InserirTaxaSalgoNegativo]
 
 											UPDATE [DBO].[Contas]
 												SET Vlr_SldInicial = '-10000'
-												WHERE Id = 6666
+												WHERE Id = 1
 
-											SELECT * FROM Lancamentos WHERE Id_Tarifa = 7
+											SELECT * FROM [DBO].[Contas]
+												WHERE Id = 1
+
+											SELECT * FROM Lancamentos
 
 											SELECT DATEDIFF(DAY, @DATA_INI, GETDATE())
 										ROLLBACK TRAN
@@ -27,26 +30,27 @@ CREATE OR ALTER TRIGGER [DBO].[TRG_InserirTaxaSalgoNegativo]
 
 	 BEGIN
 		DECLARE	@Id_CtaIN INT,
-						@ValorSaldoInicial DECIMAL(15,2),
-						@Id_CtaDE INT,
-						@Taxa DECIMAL(15,4)
+				@ValorSaldoInicialIN DECIMAL(15,2),
+				@ValorSaldoInicialDE DECIMAL(15,2),
+				@Id_CtaDE INT,
+				@Taxa DECIMAL(15,4)
 
-		SELECT @Id_CtaIN = Id, @ValorSaldoInicial = Vlr_SldInicial FROM inserted;
-		SELECT @Id_CtaDE = Id FROM deleted;
+		SELECT @Id_CtaIN = Id, @ValorSaldoInicialIN = Vlr_SldInicial FROM inserted;
+			SELECT @Id_CtaDE = Id, @ValorSaldoInicialDE = Vlr_SldInicial FROM deleted;
 
-		SET @Taxa = (SELECT Taxa FROM [DBO].Tarifas WITH(NOLOCK) WHERE Id = 7);
-			
-			-- Verificando se o que est� acontecendo � um update
-			IF @Id_CtaDE IS NOT NULL AND @Id_CtaIN IS NOT NULL
-				BEGIN
-					-- Caso o Saldo inicial daquele registro que est� sendo atualizado seja < 0, dever� ser lan�ado uma taxa de juros por dia naquele registro
-					IF @ValorSaldoInicial < 0
+		IF @ValorSaldoInicialIN <> @ValorSaldoInicialDE
+			BEGIN
+				SET @Taxa = (SELECT Taxa FROM [DBO].Tarifas WITH(NOLOCK) WHERE Id = 7);			
+					-- Verificando se o que está acontecendo é um update
+					IF @Id_CtaDE IS NOT NULL AND @Id_CtaIN IS NOT NULL
 						BEGIN
-							-- Inserir um lan�amento para todas as contas com saldo inicial negativo
-							INSERT Lancamentos (Id_Cta, Id_Usuario, Id_Tarifa, Tipo_Lanc, Vlr_Lanc, Nom_Historico, Dat_Lancamento) VALUES
-							(@Id_CtaIN, 1, 7, 'D', (@Taxa * @ValorSaldoInicial * (-1)), 'Taxa de saldo negativo', GETDATE())
+							-- Caso o Saldo inicial daquele registro que está sendo atualizado seja < 0, deverá ser lançado uma taxa de juros por dia naquele registro
+							IF @ValorSaldoInicialIN < 0
+								BEGIN									
+									-- Inserir um lançamento para todas as contas com saldo inicial negativo
+									INSERT Lancamentos (Id_Cta, Id_Usuario, Id_Tarifa, Tipo_Lanc, Vlr_Lanc, Nom_Historico, Dat_Lancamento) VALUES
+									(@Id_CtaIN, 1, 7, 'D', (@Taxa * @ValorSaldoInicialIN * (-1)), 'Taxa de saldo negativo', GETDATE())
+								END
 						END
 				END
 		END
-
-
