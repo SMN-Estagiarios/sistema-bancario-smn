1d780e1b0df3695fe4306b42c0c96b8a89e89c0e
Gustavo Targino <gustavo.targinocc@hotmail.com>
2024-04-18 09:01:34 -0300

    FEAT: Mudanca atualiza credit score

--- JOBS/SPJob_AtualizaCreditScore.sql
+++ JOBS/SPJob_AtualizaCreditScore.sql
@@ -1,10 +1,10 @@
 CREATE OR ALTER PROCEDURE [dbo].[spjob_AtualizarCreditScore]
     AS
 		/*
-		Documentação
+		DocumentaÃ§Ã£o
 			Arquivo Fonte.....: SPJob_AtualizarCreditScore.sql
 			Objetivo..........: Atualizar o CreditScore das Contas
-			Autor.............: Gustavo Targino, João Victor Maia, Gabriel Damiani
+			Autor.............: Gustavo Targino, Joï¿½o Victor Maia, Gabriel Damiani
 			Data..............: 16/04/2024
 			EX................:	BEGIN TRAN
 									
@@ -48,12 +48,12 @@ CREATE OR ALTER PROCEDURE [dbo].[spjob_AtualizarCreditScore]
                     CROSS JOIN [dbo].[Contas] c WITH(NOLOCK)
             ) s
         LEFT OUTER JOIN    (
-                            SELECT    x.Id_Cta, --Somar todos os créditos e todos os débitos após a data
+                            SELECT    x.Id_Cta, --Somar todos os crï¿½ditos e todos os dï¿½bitos apï¿½s a data
                                     x.DataSaldo,
                                     SUM(CASE WHEN x.TipoLancamento = 'C' THEN x.Vlr_Lanc ELSE 0 END) AS Credito,
                                     SUM(CASE WHEN x.TipoLancamento = 'D' THEN x.Vlr_Lanc ELSE 0 END) AS Debito
                                 FROM (
-                                        SELECT    td.DataSaldo, --Listar todos os lançamentos feitos após a data, por exemplo, DataSaldo 28 irá ter os lançamentos do dia 30 e 29
+                                        SELECT    td.DataSaldo, --Listar todos os lanï¿½amentos feitos apï¿½s a data, por exemplo, DataSaldo 28 irï¿½ ter os lanï¿½amentos do dia 30 e 29
                                                 la.Dat_Lancamento,
                                                 la.ID_Cta,
                                                 ISNULL(la.Tipo_Lanc, 'X') as TipoLancamento,
@@ -77,7 +77,7 @@ CREATE OR ALTER PROCEDURE [dbo].[spjob_AtualizarCreditScore]
                   AND C.Ativo = 'S'
             GROUP BY SD.ID_Conta
 			) UPDATE Contas
-				SET IdCreditScore = CASE WHEN ISNULL(MSM.MediaSaldoMensal, 0) > CS.Faixa THEN (SELECT MAX(Id) FROM CreditScore WHERE MSM.MediaSaldoMensal > Faixa) ELSE 1 END
+				SET IdCreditScore = CASE WHEN MSM.MediaSaldoMensal > CS.Faixa THEN (SELECT MAX(Id) FROM CreditScore WHERE MSM.MediaSaldoMensal > Faixa) ELSE 1 END
 					FROM Contas
 						INNER JOIN MediaSaldoMensal MSM
 							ON MSM.ID_Conta = Contas.ID
@@ -88,4 +88,3 @@ CREATE OR ALTER PROCEDURE [dbo].[spjob_AtualizarCreditScore]
 
     END
 GO
-
