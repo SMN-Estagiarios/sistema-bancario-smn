5fa8df04beadc4baf317025d7644b8d0652a8598
Orcino Ferreira de Lima Neto <129555010+OrcinoNeto@users.noreply.github.com>
2024-04-19 13:32:43 -0300

    Update Lancamentos.sql
    Adicionando Null quando digitado para o parametro @Dat_Lancamento receber GETDATE().

--- PROCEDURES/Lancamentos.sql
+++ PROCEDURES/Lancamentos.sql
@@ -13,37 +13,36 @@ CREATE OR ALTER PROCEDURE [dbo].[SP_CriarLancamentos]
 		@Estorno BIT
 	AS
 		/*
-			Documentação
-			Arquivo Fonte.....: Lancamentos.sql
-			Objetivo..........: Inserir Dados na Tabela Lançamentos, e não permitir lançamentos futuros
-			Autor.............: Orcino Neto, Isabella Siqueira, Thays Carvalho
- 			Data..............: 18/04/2024
-			Ex................:	BEGIN TRAN
-									DBCC DROPCLEANBUFFERS; 
-									DBCC FREEPROCCACHE;
-
-									DECLARE @Dat_init DATETIME = GETDATE(),
-                                            @RET INT,
-											@DataTeste DATETIME = GETDATE()
-
-									SELECT TOP 10 * FROM Lancamentos
-
-									EXEC @RET = [dbo].[SP_CriarLancamentos]	1, 1, 1, 1, 'C', 100, 'Deposito', @DataTeste, 0
-									SELECT TOP 10 * FROM Lancamentos
-
-                                    SELECT @RET AS RETORNO
-
-									SELECT DATEDIFF(MILLISECOND, @Dat_init, GETDATE()) AS TempoExecucao
-
-								ROLLBACK TRAN	
+		Documentação
+		Arquivo Fonte..: Lancamentos.sql
+		Objetivo..........: Inserir Dados na Tabela Lançamentos, não permitir lançamentos futuros, nem retroativos de meses passados.
+				     Digitar Null no paramentro @Dat_Lancamento ira receber GETDATE().
+		Autor..............: Orcino Neto, Isabella Siqueira, Thays Carvalho
+		Data...............: 18/04/2024
+		Ex..................:	
+					BEGIN TRAN
+						DBCC DROPCLEANBUFFERS; 
+						DBCC FREEPROCCACHE;
+	
+						DECLARE @Dat_init DATETIME = GETDATE(),
+								@RET INT
+						SELECT TOP 10 * FROM Lancamentos
+	
+						EXEC @RET = [dbo].[SP_CriarLancamentos]	1, 1, 1,1, 'C', 100, 'Deposito', null, 0
+						SELECT TOP 10 * FROM Lancamentos
+	
+						SELECT @RET AS RETORNO
+	
+						SELECT DATEDIFF(MILLISECOND, @Dat_init, GETDATE()) AS TempoExecucao
+					ROLLBACK TRAN	
                                     
-			    Lista de retornos:
+		    Lista de retornos:
                     0: Sucesso ao inserir o lançamento.
                     1: Valor de Lançamento tem que ser maior que 0.
                     2: Não permitido lançamentos futuros.
                     3: Não permitido lançamentos de meses diferentes.
                     4: Erro ao inserir lançamento.
-		*/
+		    */
 
 	BEGIN
 		-- Caso Valor do Lançamento seja menor que 0:
@@ -63,10 +62,22 @@ CREATE OR ALTER PROCEDURE [dbo].[SP_CriarLancamentos]
 			END
 		-- Caso a checagem tiver correta:
 		ELSE	
-			INSERT INTO [dbo].[Lancamentos] (Id_Cta, Id_Usuario, Id_TipoLancamento, Id_Tarifa, Tipo_Operacao, Vlr_Lanc, Nom_Historico, Dat_Lancamento, Estorno) VALUES 
-											(@Id_Cta, @Id_Usuario, @Id_TipoLancamento, @Id_Tarifa, @Tipo_Operacao, @Vlr_Lanc, @Nom_Historico, @Dat_Lancamento, @Estorno)
-            IF @@ROWCOUNT <> 0
-                RETURN 0 
-            ELSE 
-                RETURN 4
+			-- Caso paramentro seja NULL sera atribuido a variavel @DataAtual para recerber GETDATE
+			IF @Dat_Lancamento IS NULL
+				BEGIN
+					DECLARE @DataAtual DATETIME
+					SET @DataAtual = GETDATE()
+	
+					INSERT INTO [dbo].[Lancamentos] (Id_Cta,Id_Usuario,Id_TipoLancamento,Id_Tarifa,Tipo_Operacao,Vlr_Lanc,Nom_Historico,Dat_Lancamento,Estorno) VALUES 
+									(@Id_Cta, @Id_Usuario,@Id_TipoLancamento,@Id_Tarifa,@Tipo_Operacao,@Vlr_Lanc,	@Nom_Historico,@DataAtual, @Estorno)
+				END
+	
+			ELSE
+				INSERT INTO [dbo].[Lancamentos]  (Id_Cta,Id_Usuario,Id_TipoLancamento,Id_Tarifa,Tipo_Operacao,Vlr_Lanc,Nom_Historico,Dat_Lancamento,Estorno) VALUES 
+								 (@Id_Cta, @Id_Usuario,@Id_TipoLancamento,@Id_Tarifa,@Tipo_Operacao,@Vlr_Lanc,	@Nom_Historico,@DataAtual, @Estorno)
+          
+		  IF @@ROWCOUNT <> 0
+                	RETURN 0 
+            	  ELSE 
+                	RETURN 4
 	END;
